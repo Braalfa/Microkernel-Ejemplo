c2a0c10ccdce75aa89459c8e4aca2b7ccdbce9f5
Bratan <alfabrayan12@gmail.com>
2021-10-15 19:22:38 -0600

    Microkernel

---
+++ calculator.py
@@ -0,0 +1,23 @@
+import subprocess
+
+plugins = {
+    'resta' : 'subs.py',
+    'multiplicacion' : 'mul.py',
+}
+
+op1 = int(input("Cual es el operando 1?"))
+op2 = int(input("Cual es el operando 2?"))
+
+operacion = input("Que operacion desea realizar?")
+
+if operacion == "suma":
+    print("El resultado es " + str(op1+op2))
+else:
+    plugin = plugins.get(operacion)
+    if plugin != None:
+        proc = subprocess.Popen(['python', plugin, str(op1), str(op2)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = proc.communicate()[0]
+        print(result.decode("utf-8")[:-2])
+    else:
+        print('Operacion no soportada')
+
---
+++ subs.py
@@ -0,0 +1,6 @@
+import sys
+
+op1 = int(sys.argv[1])
+op2 = int(sys.argv[2])
+
+print("El resultado es " + str(op1-op2))
